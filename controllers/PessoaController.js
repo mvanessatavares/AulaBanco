bf0857f15c8df863c3e072f10b1e8ce1aa420e17
Vanessa Lira <vanessa.maria@academico.ifpb.edu.br>
2022-07-20 14:37:48 -0300

    Add files via upload

---
+++ controllers/PessoaController.js
@@ -0,0 +1,35 @@
+const { response } = require('express');
+const Pessoa = require('../models/pessoa');
+
+const buscarPessoa = async (request, response) =>{
+
+};
+
+
+const getPessoas = async (request, response)=>{
+    
+};
+
+const addPessoa = async (request, response)=>{
+    const pessoa = new Pessoa(request.body);
+    pessoa.save().then(()=>{
+        response.status(200).send('Salvo com sucesso');
+    }).catch(err=>{
+        response.status(400).send('Falha ao salvar');
+    });
+    
+};
+
+const deletarPessoa = async (request, response)=>{
+    
+};
+
+const atualizarPessoa = async (request, response)=>{
+    
+};
+
+
+
+
+
+module.exports = {getPessoas, addPessoa, deletarPessoa, atualizarPessoa, buscarPessoa};
---
+++ database/db.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`);
+}
+
+module.exports = mongoose;
